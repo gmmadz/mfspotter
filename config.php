42ab4f3ec9bfb95efce0f07d2e31ec130be98145
gmmadz <gm.madz@gmail.com>
2016-07-20 13:51:26 +0800

    transferred files to usbwebserver

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbhost="localhost"; // hostname
+$dbuser="root"; // username 
+$dbpass="usbw"; // password
+$dbname="mfspotter"; // database name
+
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass);
+mysqli_select_db($conn,$dbname);
+
+if(!$conn)
+	die('Could not connect to MySQL: ' . mysqli_error());
+
+?>
