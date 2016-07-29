313771e1fadb998ee44e257c0189eb4e17fd8c06
gmmadz <gm.madz@gmail.com>
2016-07-29 09:25:39 +0800

    working xml file

--- googlemap/display.php
+++ googlemap/display.php
@@ -1,33 +1,94 @@
+
 <?php
 
-require("phpsqlsearch_dbinfo.php");
+$username="root";
+$password="usbw";
+$database="mfspotter";
 
-$dom = new DOMDocument("1.0");
-$node = $dom->createElement("markers");
-$parnode = $dom->appendChild($node);
+$center_lat = $_GET["lat"];
+$center_lng = $_GET["lng"];
+$radius = $_GET["radius"];
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
 
+// Opens a connection to a MySQL server
+$connection=mysql_connect ('localhost', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
 
+// Set the active MySQL database
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
 
-  function fetch_area() {
-    global $dom, $node, $parnode;
-    $query = $this->link->query("SELECT * FROM markers WHERE 1");  
-    $query->setFetchMode(PDO::FETCH_ASSOC);
+// Select all the rows in the markers table
 
-    header("Content-type: text/xml");
+/*$query = sprintf("SELECT address, facilityName, latitude, longhitude, ( 3959 * acos( cos( radians('%s') ) * cos( radians( latitude ) ) * cos( radians( longhitude ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( latitude ) ) ) ) AS distance FROM facility HAVING distance < '%s' ORDER BY distance LIMIT 0 , 20";
+*/
+// Search the rows in the markers table
+$query = sprintf("SELECT address, facilityName, latitude, longhitude, ( 3959 * acos( cos( radians('%s') ) * cos( radians( latitude ) ) * cos( radians( longhitude ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( latitude ) ) ) ) AS distance FROM facility HAVING distance < '%s' ORDER BY distance LIMIT 0 , 20",
+  mysql_real_escape_string($center_lat),
+  mysql_real_escape_string($center_lng),
+  mysql_real_escape_string($center_lat),
+  mysql_real_escape_string($radius));
+$result = mysql_query($query);
+/*
+ 
+$query = " SELECT z.facilityname,
+        z.latitude, z.longhitude,
+        p.distance_unit
+                 * DEGREES(ACOS(COS(RADIANS(p.latpoint))
+                 * COS(RADIANS(z.latitude))
+                 * COS(RADIANS(p.longpoint) - RADIANS(z.longhitude))
+                 + SIN(RADIANS(p.latpoint))
+                 * SIN(RADIANS(z.latitude)))) AS distance_in_km
+  FROM facility AS z
+  JOIN (   
+        SELECT  7.1907  AS latpoint,  125.4553 AS longpoint,
+                25.0 AS radius,      111.045 AS distance_unit
+    ) AS p ON 1=1
+  WHERE z.latitude
+     BETWEEN p.latpoint  - (p.radius / p.distance_unit)
+         AND p.latpoint  + (p.radius / p.distance_unit)
+    AND z.longhitude
+     BETWEEN p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+         AND p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+  ORDER BY distance_in_km
+  LIMIT 10";
+*/
 
-        while($row = $query->fetch()) {  
-          $node = $dom->createElement("marker");  
-          $newnode = $parnode->appendChild($node);   
-          $newnode->setAttribute("name",$row['name']);
-          $newnode->setAttribute("adress", $row['adress']);  
-          $newnode->setAttribute("lat", $row['lat']);  
-          $newnode->setAttribute("lng", $row['lng']);  
-          $newnode->setAttribute("type", $row['type']);
-        } 
-   }
-  }
-$area = new Areas();
-$area_info = $area->fetch_area(); 
-echo $dom->saveXML();
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['facilityName']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['latitude'] . '" ';
+  echo 'lng="' . $row['longhitude'] . '" ';
+  echo 'type="ba" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
 
 ?>
