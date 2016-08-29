dd3c53550122a9ebea43c9ea62a4f012e5b15e2a
gmmadz <gm.madz@gmail.com>
2016-08-30 01:03:02 +0800

    evaluation chuchu

--- searchByInsurances_Table.php
+++ searchByInsurances_Table.php
@@ -46,5 +46,59 @@ if(mysqli_num_rows($result) > 0)
  }  
 
 
+//SAMPLE
+    /*$connect = mysqli_connect("localhost", $username, $password, $database);  
+    $i = 1;
+    $query = "SELECT f.facilityName AS Facility_Name, GROUP_CONCAT(i.insuranceName SEPARATOR ', ') As Insurances_Covered
+              FROM facility f, insurances i, insurancesCovered ic
+              WHERE f.facilityID = ic.facilityID AND i.insurancesID = ic.insuranceID
+              AND i.insurancesID IN (1,2,3)
+              GROUP BY f.facilityName";
+
+     $result = mysqli_query($connect, $query); 
+     echo "<div class = 'row'>";
+    if(mysqli_num_rows($result) > 0)  
+     {  
+        
+          while($row = mysqli_fetch_array($result))
+          { ?>
+            <div class="col-md-3">
+                         <div class="box box-widget widget-user ">
+                            <div class="widget-user-header bg-green">
+                              <h2 class="widget-user-username "><?php echo $row['Facility_Name'] ?></h2>
+                              <h5 class="widget-user-desc "><?php echo $row['Insurances_Covered'] ?></h5>
+                            </div>
+                          <div class="box-footer no-padding">
+                            <ul class="nav nav-stacked">
+                              
+                              <li><a href="#">Overall Ratings:
+                                <select class="rating-display-overall pull-right" data-current-rating=<?php echo $overallRating?>>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select></a>
+                              </li>
+                              <li><a href="#">Avegage Overall Rating: <span class="value"><?php echo $overallRating?></span></a></li>
+                              <li><a href="#">View Map <i class="fa fa-map-marker" aria-hidden="true"></i> </a></li>
+                              <li><a href="#">More details <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                          </div>
+                       </div>
+              </div>
+            
+            <?php 
+            if($i % 3 == 3) echo "</div> <div class = 'row'>";
+            $i++;
+          }
+        
+
+     }  
+     else  
+     {  
+          echo 'Data Not Found';  
+ }
+*/
 
 ?>
